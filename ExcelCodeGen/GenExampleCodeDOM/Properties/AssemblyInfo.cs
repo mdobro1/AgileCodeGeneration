c1012b2b32f1701e1b9e880129a80918685143ec
Mykola Dobrochynskyy <ceo@soft-fact.de>
2021-04-08 17:41:35 +0200

    Initial

---
+++ ExcelCodeGen/GenExampleCodeDOM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("GenExampleCodeDOM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenExampleCodeDOM")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("e205bb6a-4a0c-4283-987a-1600fb3b8394")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExcelCodeGen/GenExampleT4Model/RectangleModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenExampleT4Model
+{
+    public static class RectangleModel
+    {
+        public const string Namespace = "T4Sample";
+        public const string Class = "T4SampleRectangle";
+        public const string Types = "double,double";
+        public const string Fields = "width,height";
+        public const string Properties = "Width,Height";
+        public const string InitialValues = "5.3D,6.9D";
+        public const string AreaFormula = "Width * Height";
+
+        public static void Test()
+        {
+            foreach (var type in Types.Split(','))
+            {
+                int[] i = default(int[]);
+                //i.Length    
+
+            }
+        }
+    }
+}
+
+/*
+namespace CodeDOMSample 
+{
+    using System;   
+    
+    public sealed class CodeDOMCreatedClass
+    {
+        
+        // The width of the object.
+        private double widthValue;
+        
+        // The height of the object.
+        private double heightValue;
+        
+        public CodeDOMCreatedClass(double width, double height)
+        {
+            this.widthValue = width;
+            this.heightValue = height;
+        }
+        
+        // The Width property for the object.
+        public double Width
+        {
+            get
+            {
+                return this.widthValue;
+            }
+        }
+        
+        // The Height property for the object.
+        public double Height
+        {
+            get
+            {
+                return this.heightValue;
+            }
+        }
+        
+        // The Area property for the object.
+        public double Area
+        {
+            get
+            {
+                return (this.widthValue * this.heightValue);
+            }
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("The object:\r\n width = {0},\r\n height = {1},\r\n area = {2}", this.Width, this.Height, this.Area);
+        }
+        
+        public static void Main()
+        {
+            CodeDOMCreatedClass testClass = new CodeDOMCreatedClass(5.3D, 6.9D);
+            System.Console.WriteLine(testClass.ToString());
+        }
+    }
+}
+*/
